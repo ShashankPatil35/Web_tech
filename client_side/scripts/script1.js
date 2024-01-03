6309943f59716b8f504aa95815b31d9d16091f94
shashank <shashankmpatil3535@gmail.com>
2024-01-03 16:25:42 +0530

    done# Please enter the commit message for your changes. Lines starting

--- client_side/scripts/script1.js
+++ client_side/scripts/script1.js
@@ -2,3 +2,34 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0)
 })
+
+
+const input = document.querySelector(".input__field");
+
+const inputIcon = document.querySelector(".input__icon");
+
+inputIcon.addEventListener("click", (e) => {
+
+e.preventDefault();
+
+inputIcon.setAttribute(
+
+'src',
+
+input.getAttribute('type')==='password' ?
+
+'eye-off.svg'
+:
+'eye.svg'
+);
+input.setAttribute(
+
+'type',
+
+input.getAttribute('type')==='password' ?
+
+'text'
+:
+'password'
+);
+});
